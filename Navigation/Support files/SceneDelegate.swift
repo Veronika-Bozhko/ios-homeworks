302acdd73911b01f8b4164e403e087deca097aa7
Veronika Bozhko <valriria@mail.ru>
2023-03-01 16:39:04 +0300

    Add HW-3

--- Navigation/Support files/SceneDelegate.swift
+++ Navigation/Support files/SceneDelegate.swift
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
-//        print(#function)
+        print(#function)
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
         // Release any resources associated with this scene that can be re-created the next time the scene connects.
